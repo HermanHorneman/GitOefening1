af43a222237891ed3a24af004c4a19c11f29b523
Herman Horneman <h.horneman@students.uu.nl>
2019-11-20 11:27:43 +0100

    Test 1

---
+++ GitOefening/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitOefening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
